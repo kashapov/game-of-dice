e4924a540a185056422c4977090743f8456cc002
Andrey Kashapov <kashapovandrey@gmail.com>
2018-07-09 13:53:47 +0300

    add autorization for git

--- app.js
+++ app.js
@@ -57,6 +57,8 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     //document.querySelector('.player-1-panel').classList.add('active');
 
     document.querySelector('.dice').style.display = 'none';
+
+
   }
 
 });
